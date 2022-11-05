ea645431973413a1741a3d01d4419d66874a49b1
YahooBaba <YahooBaba@gmail.com>
2022-11-05 16:49:58 +0500

    inital commit

---
+++ Infoguards/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Infoguards.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult HTTP()
+        {
+            ViewBag.Message = "Your HTTP page.";
+            return View();
+        }
+
+        public ActionResult HTTPS()
+        {
+            ViewBag.Message = "Your HTTPS page.";
+
+            return View();
+        }
+
+        public ActionResult NetMVC()
+        {
+            ViewBag.Message = "Your NetMVC page.";
+
+            return View();
+        }
+
+        public ActionResult NetCore()
+        {
+            ViewBag.Message = "Your NetCore page.";
+
+            return View();
+        }
+    }
+}
---
+++ Infoguards/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Infoguards.MvcApplication" Language="C#" %>
